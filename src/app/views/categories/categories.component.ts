1849d794f4872be1c062122f507a5e7ad967dea8
Zeegit <sergey.zemskov@gmail.com>
2020-10-12 00:44:22 +0300

    добавил список категорий

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from '../../service/data-handler.service';
+import {Category} from '../../model/Category';
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+    selector: 'app-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+    categories: Category[];
 
-  constructor() { }
+    constructor(private dataHandler: DataHandlerService) {
+    }
 
-  ngOnInit(): void {
-  }
+    // Вызывается после создания компонента
+    ngOnInit(): void {
+        this.categories = this.dataHandler.getCategories();
+        console.log(this.categories);
+    }
 
 }
